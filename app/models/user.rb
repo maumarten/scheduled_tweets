04075e5524acea1cd929d9a9c4b10b22c18bb786
maumarten <mau_marten95@hotmail.com>
2021-10-16 11:20:35 -0600

    finished base app

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+# email:string
+# password_digest:string
+#
+# password:string virtual
+# password_confirmation:string virtual
+
+class User < ApplicationRecord
+  has_secure_password
+
+  has_many :twitter_accounts
+  has_many :tweets
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+end
